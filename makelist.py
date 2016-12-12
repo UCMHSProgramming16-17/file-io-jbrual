9dddb74d5d0f9724ccfa61b7389a7c5aec58f1da
Jeric Brual <jbrual@ucvts.org>
2016-12-12 18:24:13 +0000

    Edited makelist.py.

--- makelist.py
+++ makelist.py
@@ -3,6 +3,7 @@ file = open("UserData.txt", "w")
 
 # Create the list.
 for n in range(10):
-    file.write(input("Speak! " + "\n")
+    file.write(input("Speak! " + "\n"))
     
 # Close file.
+file.close()
