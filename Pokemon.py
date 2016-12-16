50b3ea1e9cf54e8885d126325b46c648f3482b3f
Jeric Brual <jbrual@ucvts.org>
2016-12-16 18:08:16 +0000

    Added files.

--- Pokemon.py
+++ Pokemon.py
@@ -2,6 +2,24 @@ import csv
 import requests
 
 # Q: What are the fastest Flying-type Pokemon?
+base_url = "http://pokeapi.co/api/v2/"
+type_url = base_url + "type/"
+r = requests.get(type_url + "flying/")
 
-csvfile = open("PokeData.csv", "w")
+results = r.json()
+flying_list = results["pokemon"]
+
+csvfile = open("Flying.csv", "w")
 csvwriter = csv.writer(csvfile, delimiter=',')
+    
+for pokemon in flying_list:
+    name = pokemon["pokemon"]["name"]
+    print(name)
+    csvwriter.writerow([name])
+    
+# for pokemon in flying_list:
+#     speed = pokemon["pokemon"]["url"]
+#     for url in speed:
+#         print(["speed"])
+    
+csvfile.close()
