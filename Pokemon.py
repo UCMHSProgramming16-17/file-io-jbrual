70c2bb8db15f8ca41040db869874663c16114343
Jeric Brual <jbrual@ucvts.org>
2016-12-20 03:21:16 +0000

    Updated 'Pokemon.py'

--- Pokemon.py
+++ Pokemon.py
@@ -1,20 +1,25 @@
-import csv
-import requests
-import operator
-
 # Q: What are the fastest Flying-type Pokemon?
+# Import the appropriate modules.
+import csv, requests, operator
+
+# Reach the API by building its URL.
 base_url = "http://pokeapi.co/api/v2/"
 type_url = base_url + "type/"
 r = requests.get(type_url + "flying/")
 
+# Use .json format and use the appropriate dict. key.
 results = r.json()
 flying_list = results["pokemon"]
 
+# Create a .csv file for export data.
 csvfile = open("Flying.csv", "w")
 csvwriter = csv.writer(csvfile, delimiter = ",")
 
+# Establish header row.
 csvwriter.writerow(["Pokemon", "Speed"])
-    
+
+# For each Pokemon, determine its name and base speed stat
+# and write it to the .csv.
 for pokemon in flying_list:
     name = pokemon["pokemon"]["name"]
     url = pokemon["pokemon"]["url"]
@@ -24,11 +29,9 @@ for pokemon in flying_list:
     speed = speed_list["stats"][0]["base_stat"]
     csvwriter.writerow([name, speed])
 
-csvsort = open("Flying.csv", "r")
-csvreader = csv.reader(csvsort, delimiter = ",")
-sort = sorted(csvreader,key=operator.itemgetter(1))
+data = csv.reader(open("Flying.csv"), delimiter = ",")
+sortedlist = sorted(data, key = operator.itemgetter(1))
+csvwriter.writerow(sortedlist)
 
-for line in sort:
-    csvwriter.writerow([name, speed])
-
+# Close the .csv.
 csvfile.close()
