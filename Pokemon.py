e0525356d8de8ab1f9b0e0fbb07cb7b4cbd6cec9
Jeric Brual <jbrual@ucvts.org>
2016-12-20 01:14:20 +0000

    Updated Pokemon.py.

--- Pokemon.py
+++ Pokemon.py
@@ -1,5 +1,6 @@
 import csv
 import requests
+import operator
 
 # Q: What are the fastest Flying-type Pokemon?
 base_url = "http://pokeapi.co/api/v2/"
@@ -10,16 +11,17 @@ results = r.json()
 flying_list = results["pokemon"]
 
 csvfile = open("Flying.csv", "w")
-csvwriter = csv.writer(csvfile, delimiter=',')
+csvwriter = csv.writer(csvfile, delimiter = ",")
+
+csvwriter.writerow(["Pokemon", "Speed"])
     
 for pokemon in flying_list:
     name = pokemon["pokemon"]["name"]
-    print(name)
-    csvwriter.writerow([name])
-    
-# for pokemon in flying_list:
-#     speed = pokemon["pokemon"]["url"]
-#     for url in speed:
-#         print(["speed"])
+    url = pokemon["pokemon"]["url"]
+    rspeed = requests.get(url)
+    speed_results = rspeed.json()
+    speed_list = speed_results
+    speed = speed_list["stats"][0]["base_stat"]
+    csvwriter.writerow([name, speed])
     
 csvfile.close()
